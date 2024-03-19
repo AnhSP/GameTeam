6eefbfb533b14194ee3e02af20a4b2508e533032
AnhSP <chimpui101@gmail.com>
2024-03-20 01:29:36 +0700

    first architecture

---
+++ bai-tap-lon-java/src/main/java/com/application/test/AppRuner.java
@@ -0,0 +1,8 @@
+package com.application.test;
+
+// Run cho Game Application
+public class AppRuner {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ bai-tap-lon-java/src/main/java/com/game2d/constant/SystemConstant.java
@@ -0,0 +1,5 @@
+package com.game2d.constant;
+
+public class SystemConstant {
+    public static int FPS= 60;
+}
